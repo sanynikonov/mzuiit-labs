2ce7b3477e07b992d3e2b55967a626c4efa1e320
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2023-05-14 20:21:14 +0300

    Use admin password

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,15 +34,13 @@ resource "azurerm_linux_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
+
+  admin_username      = "notadmin"
+  admin_password      = "!tsymbalskynlaby1"
+
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
