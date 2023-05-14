7f93653b1297d3cd8268cd3ca763fe191d065a14
snizhokma <snizhokma@gmail.com>
2023-05-14 17:23:12 +0300

    Add the feature

--- html/index.js
+++ html/index.js
@@ -1,3 +1,11 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.onClickButton = () => {
-  console.log("Get 1 biscuit!");
+  const title = document.getElementById("title");
+  title.style.color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+
+  const main = document.getElementsByTagName("main")[0];
+  main.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
 }
